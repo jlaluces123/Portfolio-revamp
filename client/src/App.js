16616ec2d492083be473b8029731e69033942f27
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-08-05 11:32:35 -0700

    Navigation Menu: Working as intended, now styling and adding fonts

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,40 @@ import './App.css';
 
 // Component Imports
 import { Route } from "react-router-dom";
+import Navigation from "./components/Navigation/index.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* Navigation */}
-      </header>
 
-      <main>
-        <h3>main content</h3>
-      </main>
-    </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    visible: false,
+  };
+
+  componentDidMount() {
+    console.log("did mount");
+    // console.log(document.getElementsByClassName("hidden")[0].style.height);
+  };
+
+  handleMenu = () => {
+    this.setState({ visible: !this.state.visible });
+  };
+
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          {/* Navigation */}
+          <Navigation visible = {this.state.visible} handleMenu = {this.handleMenu} />
+        </header>
+
+        <main>
+          {/* <h3>&#9776;</h3>
+          <h3>&times;</h3> */}
+          <code>Main content</code>
+        </main>
+      </div>
+    );
+  };
+};
 
 export default App;
---
+++ client/src/components/Navigation/index.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import "./navigation.css";
+
+// Component Imports
+import { NavLink, Link } from "react-router-dom";
+
+// Assets
+
+
+const Navigation = (props) => {
+  return (
+    <div className="container">
+      <div className="navigation">
+        <Link className = "navigation__home" to = "/">Jonathan Laluces</Link>
+        <button className="navigation__btn" onClick = {props.handleMenu}>&#9776;</button>
+      </div>
+
+      <div onClick = {props.handleMenu} className = {props.visible ? "fullHeight" : "noHeight"} id = "hidden-nav">
+        {/* <button className="hidden__close" onClick = {props.handleMenu}>&times;</button> */}
+
+        <div className="hidden__container">
+          <div className="hidden__content">
+            <Link className = "hidden__btn" to = "/">Home</Link>
+            <Link className = "hidden__btn" to = "/about">About</Link>
+            <Link className = "hidden__btn" to = "/portfolio">Portfolio</Link>
+            <Link className = "hidden__btn" to = "/resume">Resume</Link>
+          </div>
+
+          <div className="hidden__contact">
+            <h3 className = "contact__header">Contact Info:</h3>
+            <p>(908) 414 3555</p>
+            <p>jlalucescareer123@gmail.com</p>
+            <p>Los Angeles, CA</p>
+
+            <div className="btn__container">
+              <li><a href="/"><img className = "social__btn" /></a></li>
+              <li><a href="/"><img className = "social__btn" /></a></li>
+              <li><a href="/"><img className = "social__btn" /></a></li>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default Navigation;
