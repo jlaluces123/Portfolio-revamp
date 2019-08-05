4bf45196a26922c387bffbb56b996554c4f18feb
Jonathan Laluces <jlalucescareer123@gmail.com>
2019-08-04 18:18:10 -0700

    CSS: commented out CSS files to start from scratch

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Component Imports
+import { Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Navigation */}
       </header>
+
+      <main>
+        <h3>main content</h3>
+      </main>
     </div>
   );
 }
