f62eb3cf8175f66c5030a6b838f19e790aa38f3d
unknown <jlalucescareer123@gmail.com>
2020-02-10 10:25:04 -0800

    Navigation Sizing Revamp: created two separate components to be rendered based on window size for navigation system

---
+++ client/src/components/Navigation/BigNav.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const BigNav = props => {
+    return <h1>Big nav</h1>;
+};
+
+export default BigNav;
--- client/src/components/Navigation/index.js
+++ client/src/components/Navigation/index.js
@@ -1,59 +1,47 @@
 import React from 'react';
-import "./navigation.css";
+import './navigation.css';
 
 // Component Imports
-import { Link } from "react-router-dom";
+import SmallNav from './SmallNav.js';
+import BigNav from './BigNav.js';
 
-// Assets
-import LinkedIn from "../../assets/linkedin.png";
-import Github from "../../assets/github_white.png";
-import Penguin from "../../assets/penguin_noun.svg";
+class Navigation extends React.Component {
+    state = {
+        visible: false,
+        width: 0
+    };
 
-const Navigation = (props) => {
-  return (
-    <div className="container">
-      <div className="navigation">
-        <Link className = "navigation__home" to = "/">
-          <img src = {Penguin} alt = "Penguin black and white logo" className = "logo"/>
-        </Link>
-        <button className="navigation__btn" onClick = {props.handleMenu}>&#9776;</button>
-      </div>
+    componentDidMount = () => {
+        this.readWindowSize();
+        window.addEventListener('resize', this.readWindowSize);
+    };
 
-      <div onClick = {props.handleMenu} className = {props.visible ? "fullHeight" : "noHeight"} id = "hidden-nav">
-        {/* <button className="hidden__close" onClick = {props.handleMenu}>&times;</button> */}
+    componentWillUnmount = () => {
+        window.removeEventListener('resize', this.readWindowSize);
+    };
 
-        <div className="hidden__container">
-          <div className="hidden__content">
-            <Link className = "hidden__btn" to = "/">Home</Link>
-            <Link className = "hidden__btn" to = "/about">About</Link>
-            <Link className = "hidden__btn" to = "/resume">Resume</Link>
-          </div>
+    handleMenu = () => {
+        this.setState({ visible: !this.state.visible });
+    };
 
-          <div className="hidden__contact">
-            <h3 className = "contact__header">Contact Info:</h3>
-            <p>(908) 414 3555</p>
-            <p>jlalucescareer123@gmail.com</p>
-            <p>Los Angeles, CA</p>
+    readWindowSize = () => {
+        console.log('detected window width: ', window.innerWidth);
+        this.setState({ width: window.innerWidth });
+    };
 
-            <div className="btn__container">
-              {/* LinkedIn */}
-              <li>
-                <a rel = "noopener noreferrer" target = "_blank" href = "https://www.linkedin.com/in/jonathan-laluces/">
-                  <img alt = "social media button: LinkedIn - Jonathan Laluces" className = "social__btn" src = {LinkedIn} />
-                </a>
-              </li>
-              {/* Github */}
-              <li>
-                <a rel = "noopener noreferrer" target = "_blank" href="https://github.com/jlaluces123">
-                  <img alt = "social media button: Github - Jonathan Laluces" className = "social__btn" src = {Github} />
-                </a>
-              </li>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    render() {
+        if (this.state.width > 768) {
+            console.log(this.state.width);
+            return <BigNav />;
+        } else {
+            return (
+                <SmallNav
+                    visible={this.state.visible}
+                    handleMenu={this.handleMenu}
+                />
+            );
+        }
+    }
 }
 
 export default Navigation;
