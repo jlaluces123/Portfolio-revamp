6dbc1082e52eeb7053ce1b62e4c718a97385db61
unknown <jlalucescareer123@gmail.com>
2020-04-16 16:10:51 -0700

    Home Page Text: small update

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,55 +1,63 @@
 import React from 'react';
-import "./home.css";
+import './home.css';
 
 // Component Imports
 // import { Link } from "react-router-dom";
-import Projects from "../Projects/index.js";
-import { projectArr } from "../Data/index.js";
+import Projects from '../Projects/index.js';
+import { projectArr } from '../Data/index.js';
 
 // Assets
-import ProfileWebP from "../../assets/cropped_smiling.webp";
-import Profile from "../../assets/cropped_smiling.jpeg";
+import ProfileWebP from '../../assets/cropped_smiling.webp';
+import Profile from '../../assets/cropped_smiling.jpeg';
 
 class Home extends React.Component {
-  state = {
-    projects: [],
-  };
+    state = {
+        projects: [],
+    };
 
-  componentDidMount() {
-    this.setState({ projects: projectArr });
-  };
+    componentDidMount() {
+        this.setState({ projects: projectArr });
+    }
 
-  componentDidUpdate() {
-    // console.log("update occurred");
-    // console.log(this.state.projects);
-  };
+    componentDidUpdate() {
+        // console.log("update occurred");
+        // console.log(this.state.projects);
+    }
 
-  render() {
-    return (
-      <div className="home">
+    render() {
+        return (
+            <div className='home'>
+                {/* Intro section */}
+                <div className='intro'>
+                    <picture>
+                        <source srcSet={ProfileWebP} type='image/webp' />
+                        <img
+                            className='intro__pic'
+                            src={Profile}
+                            alt='smiling version of me'
+                        />
+                    </picture>
+                    <div className='intro__text'>
+                        <h4 className='intro__title'>
+                            Software Engineer & Cellist
+                        </h4>
+                        <h1 className='intro__name'>I'm Jonathan Laluces</h1>
+                        <p className='info__text'>
+                            Check out my projects below or view my{' '}
+                            <a className='view__btn' href='/resume'>
+                                resume
+                            </a>
+                        </p>
+                    </div>
+                </div>
 
-        {/* Intro section */}
-        <div className="intro">
-          <picture>
-            <source srcSet = {ProfileWebP} type = "image/webp" />
-            <img className = "intro__pic" src = {Profile} alt = "smiling version of me" />
-          </picture>
-          <div className="intro__text">
-            <h4 className = "intro__title">Front-End Web Developer & Cellist</h4>
-            <h1 className = "intro__name">I'm Jonathan Laluces</h1>
-            <p className = "info__text">
-              Check out my projects below or view my <a className = "view__btn" href = "/resume">resume</a>
-            </p>
-          </div>
-        </div>
-
-        {/* Projects Section */}
-        <div className="projects">
-          <Projects projects = {this.state.projects} />
-        </div>
-      </div>
-    );
-  };
-};
+                {/* Projects Section */}
+                <div className='projects'>
+                    <Projects projects={this.state.projects} />
+                </div>
+            </div>
+        );
+    }
+}
 
 export default Home;
