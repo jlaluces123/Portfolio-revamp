7a70bf585e8be000b424ff2c4e55886b7e3747a9
Jonathan Laluces <jlalucescareer123@gmail.com>
2020-11-12 09:58:42 -0500

    Fixed Scroll: clicking on a project page now opens the page at the top instead of the previous scroll position

--- client/src/components/Project/index.js
+++ client/src/components/Project/index.js
@@ -1,25 +1,32 @@
 import React from 'react';
 
 // Component Imports
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const Project = (props) => {
-  // console.log("props from Project\n", props);
-  return (
-    // Project Beginning
-    <Link to = {props.project.link}>
-      <div className="project">
-        {/* Image here */}
-        <img className = "project__img" src = {props.project.image} alt = {props.project.description} />
+    // console.log("props from Project\n", props);
 
-        <div className="img__overlay">
-          <h3>{props.project.title}</h3>
-          <p className = "overlay__paragraph">{props.project.description}</p>
-        </div>
-      </div>
-    </Link>
-    // Project End
-  );
-}
+    return (
+        // Project Beginning
+        <a href={props.project.link}>
+            <div className='project'>
+                {/* Image here */}
+                <img
+                    className='project__img'
+                    src={props.project.image}
+                    alt={props.project.description}
+                />
+
+                <div className='img__overlay'>
+                    <h3>{props.project.title}</h3>
+                    <p className='overlay__paragraph'>
+                        {props.project.description}
+                    </p>
+                </div>
+            </div>
+        </a>
+        // Project End
+    );
+};
 
 export default Project;
